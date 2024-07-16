eed0b48e4c19ef041d29d74c9a65dffc197d391a
Philipp Sommer <95475743+psomm@users.noreply.github.com>
2024-07-16 13:25:01 +0200

    Update simple.py

--- src/sample/simple.py
+++ src/sample/simple.py
@@ -1,2 +1,13 @@
-def add_one(number):
-    return number + 1
+def add(a, b):
+    """
+    Add two numbers.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
+    
